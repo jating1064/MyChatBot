1e9bc17060852c12030a991c03ee606f688d167b
Jating1064 <jating1064@gmail.com>
2024-03-02 11:06:17 +0530

    2nd Commit

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
 
-OPENAI_API_KEY="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"
+OPENAI_API_KEY="--"
 
 
 #Upload pdf files
